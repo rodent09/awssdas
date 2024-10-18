1bffb7d248cc7135d444ceac51fe51ca37a24270
Anurodhan Pradhan <85543270+rodent09@users.noreply.github.com>
2024-10-18 11:38:27 +0100

    Create new.py

---
+++ new.py
@@ -0,0 +1,8 @@
+# Configure AWS credentials using environment variables
+aws_client = boto3.client(
+    'bedrock',
+    region_name='us-east-1',
+    aws_access_key_id=os.getenv('AKIAW3MEFUQTKL57NDLC'),
+  
+
+  
